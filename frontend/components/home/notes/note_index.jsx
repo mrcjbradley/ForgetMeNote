038330742a705cb72ada59f4e50d3e31b18ab283
mrcjbradley <cjbradley@me.com>
2019-03-31 19:01:50 -0400

    Style sorting option menu for NotesIndex

--- frontend/components/home/notes/note_index.jsx
+++ frontend/components/home/notes/note_index.jsx
@@ -11,6 +11,18 @@ class NoteIndex extends React.Component {
         getAllNotes();
     }
 
+    handleMouseEnter(e){
+        $(e.target).addClass('grey');        
+    }
+    
+    handleMouseLeave(e){
+        $(e.target).removeClass('grey');      
+    }
+
+    handleOptionClick(e){
+        $(e.target).siblings().removeClass('active');
+        $(e.target).addClass('active');
+    }
 
 
     render(){
@@ -29,10 +41,52 @@ class NoteIndex extends React.Component {
                     {notes.length} notes
                 </span>
                 <div className="NoteIndex_NoteTagsOptions">
-                    <nav className="NoteIndex_Note-options">
+                    <nav className="NoteIndex_NoteOptions">
                     <div className="bg--option-dd-icon"></div>
-                        <ul>
-                            <li></li>
+                        <ul className="NoteOptions_SortMenu">
+                        <h2 className="SortByTitle">Sort by...</h2>
+                            <li className="SortByOption" 
+                                onMouseEnter={this.handleMouseEnter} 
+                                onClick={this.handleOptionClick}
+                                onMouseLeave={this.handleMouseLeave}>
+                                Date created: Most to least recent
+                                <span className="bg--check-icon"></span>
+                            </li>
+                            <li className="SortByOption" 
+                                onMouseEnter={this.handleMouseEnter} 
+                                onClick={this.handleOptionClick}
+                                onMouseLeave={this.handleMouseLeave}>
+                                Date created: Least to most recent
+                                <span className="bg--check-icon"></span>
+                            </li>
+                            <li className="SortByOption" 
+                                onMouseEnter={this.handleMouseEnter} 
+                                onClick={this.handleOptionClick}
+                                onMouseLeave={this.handleMouseLeave}>
+                                Date updated: Most to least recent
+                                <span className="bg--check-icon"></span>
+                            </li>
+                            <li className="SortByOption" 
+                                onMouseEnter={this.handleMouseEnter} 
+                                onClick={this.handleOptionClick}
+                                onMouseLeave={this.handleMouseLeave}>
+                                Date updated: Least to most recent
+                                <span className="bg--check-icon"></span>
+                            </li>
+                            <li className="SortByOption" 
+                                onMouseEnter={this.handleMouseEnter} 
+                                onClick={this.handleOptionClick}
+                                onMouseLeave={this.handleMouseLeave}>
+                                Title: A to Z
+                                <span className="bg--check-icon"></span>
+                            </li>
+                            <li className="SortByOption" 
+                                onMouseEnter={this.handleMouseEnter} 
+                                onClick={this.handleOptionClick}
+                                onMouseLeave={this.handleMouseLeave}>
+                                Title: Z to A
+                                <span className="bg--check-icon"></span>
+                            </li>
                         </ul>
                     </nav>
                     <nav className="NoteIndex_NoteTags">
