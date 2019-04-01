20d4dd34cdfa962c45c07323d1c6fa4b4dce85d9
mrcjbradley <cjbradley@me.com>
2019-04-01 15:31:56 -0400

    Fix bugs and style Note Detail Component

--- frontend/components/home/home.jsx
+++ frontend/components/home/home.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { logOut } from '../../actions/session_actions';
 import HomeSidebar from './home_sidebar';
 import NoteIndex from './notes/note_index';
-import NotePannel from './notes/note_pannel';
 import NoteDetail from './notes/note_detail';
 import { Route } from 'react-router-dom';
 
--- frontend/components/home/notes/note_detail.jsx
+++ frontend/components/home/notes/note_detail.jsx
@@ -1,30 +1,78 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { getNote } from '../../../actions/note_actions';
+import { Link } from 'react-router-dom';
 
 class NoteDetail extends React.Component {
     constructor(props){
         super(props);
+        this.state = this.props.note;
     }
 
     componentDidMount(){
         // if (oldProps.noteId !== newProps.noteId){
         const { getNote, noteId } = this.props;
-        getNote(noteId);
+        getNote(noteId).then(({note})=> {
+            this.setState(note);
+            // debugger
+        });
         // }
     }
 
-    // componentDidUpdate(oP, nP){
+    componentDidUpdate(prevProps, prevState){
+        const { getNote, noteId, note } = this.props;
+        if (prevProps.noteId !== noteId){
+            this.setState(note);
+            // debugger
+        };
+    }
 
-    // }
+    handleChange(field){
+        return e => {
+            this.setState({[field]: e.target.value});
+        };
+    }
+
+    toggleDisabled(klass){
+        return e => {
+       $(`.${klass}`).removeAttr('disabled').focus();
+        };
+    } 
 
     render(){
         // debugger
-        const { title, content } = this.props.note;
+        const { title, content } = this.state;
         // debugger
         return(
-            <article className="NoteShow">
-                <h3>{title}</h3>
+            <article className="NoteShow" >
+                <form className="NoteShow_NoteForm">
+                    <div className="NoteDetail_NoteHeader">
+                        <nav className="NoteDetail_NoteHeaderNav">
+                            <nav className="NoteHeaderNav_left">
+                                <div className="NoteHeaderNav_Expand"></div>
+                                <Link to="#" className="NoteHeaderNav_NoteBookLink"> <span className="bg--notebook-icon"></span>First Notebook</Link>
+                            </nav>
+                            <div className="NoteHeaderNav_MoreOptions">...</div>
+                        </nav>
+                        <div className="NoteShow_ToolBar"></div>
+                        <div className="NoteDetail_DisableWrapper" onClick={this.toggleDisabled("NoteDetail_NoteTitle")}>
+                            <input type="text" 
+                                className="NoteDetail_NoteTitle" 
+                                onChange={this.handleChange('title')} 
+                                value={title} 
+                                disabled
+                            />
+                        </div>
+                    </div>
+                    <div className="NoteDetail_DisableWrapper" onClick={this.toggleDisabled("NoteDetail_NoteContent")}>
+                        <textarea 
+                        className="NoteDetail_NoteContent" 
+                        onChange={this.handleChange('content')} 
+                        value={content} 
+                        disabled>
+                        </textarea>
+                    </div>
+                </form>
             </article>
         );
     }
