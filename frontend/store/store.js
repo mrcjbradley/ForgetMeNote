ccd6805b9843bebcf9a42b3ce47afd0ea3ee11c3
mrcjbradley <cjbradley@me.com>
2019-04-25 15:41:10 -0400

    Update production env for heroku

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -1,18 +1,34 @@
 import { 
     createStore,
     applyMiddleware,
-    compose
+    // compose
 } from 'redux';
 import thunk from 'redux-thunk';
-import { logger } from 'redux-logger';
+// import { logger } from 'redux-logger';
 import rootReducer from '../reducers/root_reducer';
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+// const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-export default (preloadedState = {}) => (
-    createStore(
-        rootReducer,
-        preloadedState,
-        composeEnhancers(applyMiddleware(thunk, logger))
-    )
+// export default (preloadedState = {}) => (
+//     createStore(
+//         rootReducer,
+//         preloadedState,
+//         composeEnhancers(applyMiddleware(thunk, logger))
+//     )
+// );
+
+const middlewares = [thunk];
+
+if (process.env.NODE_ENV !== 'production') {
+    // must use 'require' (import only allowed at top of file)
+    const {
+        logger
+    } = require('redux-logger');
+    middlewares.push(logger);
+}
+
+const configureStore = (preloadedState = {}) => (
+    createStore(rootReducer, preloadedState, applyMiddleware(...middlewares))
 );
+
+export default configureStore;
